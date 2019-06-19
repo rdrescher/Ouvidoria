664811e1a2b4f6f42ed3e783a36f24dc539d27f1
Rhuan <rhuan.drescher@gmail.com>
2019-06-19 20:52:28 -0300

    Notificator

---
+++ src/Ouvidoria.Domain/Notificacoes/Notificador.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ouvidoria.Domain.Interfaces;
+
+namespace Ouvidoria.Domain.Notificacoes
+{
+    public class Notificador : INotificador
+    {
+        private List<Notificacao> Notificacoes;
+
+        public Notificador()
+        {
+            Notificacoes = new List<Notificacao>();
+        }
+
+        public void Handle(Notificacao notificacao)
+        {
+            Notificacoes.Add(notificacao);
+        }
+
+        public List<Notificacao> GetNotifications()
+        {
+            return Notificacoes;
+        }
+
+        public bool HasNotification()
+        {
+            return Notificacoes.Any();
+        }
+    }
+}
